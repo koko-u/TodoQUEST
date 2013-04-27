5f54190106c76d4a62914a7b0c522b9259a19f19
phasmatodean <afuroman3104614@gmail.com>
2013-04-27 16:40:30 +0900

    コメント

--- js/quest.js
+++ js/quest.js
@@ -1,33 +1,20 @@
-
-//たたかうボタンプッシュ時の処理
-function battleBTN_pushed(){
-	$("#todomenu").css("visibility","visible");
-	$("#todomenu").empty();//emptyで子要素の削除
-	
-	// for(var i:number=0; i < todo.length; i++){
-	// 	$("#todomenu").append("<li>" + todo[i] + "</li>");
-	// }
-	$("#todomenu").append("<li>TODOTODOTODO</li>");
+function battleBTN_pushed() {
+    $("#todomenu").css("visibility", "visible");
+    $("#todomenu").empty();
+    $("#todomenu").append("<li>仕事をする</li>");
 }
-
-//逃げるボタンプッシュ時の処理
-function escapeBTN_pushed(){
-	$("#todomenu").css("visibility","visible");
-	$("#todomenu").empty();//emptyで子要素の削除
-	$("#todomenu").append("<li>ToDoからは　にげられない</li>");
+function escapeBTN_pushed() {
+    $("#todomenu").css("visibility", "visible");
+    $("#todomenu").empty();
+    $("#todomenu").append("<li>ToDoからは　にげられない</li>");
 }
-
-//防御ボタンプッシュ時の処理
-function itemBTN_pushed(){
-	$("#todomenu").css("visibility","visible");
-	$("#todomenu").empty();//emptyで子要素の削除
-// 	$("#todomenu").append("<li>しかし回り込まれてしまった。</li>");
+function itemBTN_pushed() {
+    $("#todomenu").css("visibility", "visible");
+    $("#todomenu").empty();
 }
-
-//道具ボタンプッシュ時の処理
-function magicBTN_pushed(){
-	$("#todomenu").css("visibility","visible");
-	$("#todomenu").empty();//emptyで子要素の削除
-	$("#todomenu").append("<li>ゆうしゃは　メラゾーマをとなえた。</li>");
-	$("#todomenu").append("<li>しかし　なにもおきなかった</li>");
+function magicBTN_pushed() {
+    $("#todomenu").css("visibility", "visible");
+    $("#todomenu").empty();
+    $("#todomenu").append("<li>ゆうしゃは　メラゾーマをとなえた。</li>");
+    $("#todomenu").append("<li>しかし　なにもおきなかった。</li>");
 }
