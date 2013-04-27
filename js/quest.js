40c548b154a8ce2a0a89b8fdbf0a9e56536e385b
phasmatodean <afuroman3104614@gmail.com>
2013-04-27 14:39:30 +0900

    変更済み

--- js/quest.js
+++ js/quest.js
@@ -1,4 +1,19 @@
-//
-function btn_pushed():void{
-	
+//たたかうボタンプッシュ時の処理
+function battleBTN_pushed():void{
+	 
+}
+
+//逃げるボタンプッシュ時の処理
+function escapeBTN_pushed():void{
+
+}
+
+//防御ボタンプッシュ時の処理
+function diffenceBTN_pushed():void{
+
+}
+
+//道具ボタンプッシュ時の処理
+function itemBTN_pushed():void{
+
 }
