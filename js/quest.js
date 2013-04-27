216ae06dd4443b5b7deb44d66290930736477d2e
phasmatodean <afuroman3104614@gmail.com>
2013-04-27 15:35:41 +0900

    toDoの表示を完了 魔法や逃げるコマンドの文字の表示も完了

--- js/quest.js
+++ js/quest.js
@@ -1,20 +1,33 @@
 
 //たたかうボタンプッシュ時の処理
-function battleBTN_pushed():void{
-	 $("#todomenu").css("visibility","visible")
+function battleBTN_pushed(){
+	$("#todomenu").css("visibility","visible");
+	$("#todomenu").empty();//emptyで子要素の削除
+	
+	// for(var i:number=0; i < todo.length; i++){
+	// 	$("#todomenu").append("<li>" + todo[i] + "</li>");
+	// }
+	$("#todomenu").append("<li>TODOTODOTODO</li>");
 }
 
 //逃げるボタンプッシュ時の処理
-function escapeBTN_pushed():void{
-
+function escapeBTN_pushed(){
+	$("#todomenu").css("visibility","visible");
+	$("#todomenu").empty();//emptyで子要素の削除
+	$("#todomenu").append("<li>ToDoからは　にげられない</li>");
 }
 
 //防御ボタンプッシュ時の処理
-function diffenceBTN_pushed():void{
-
+function itemBTN_pushed(){
+	$("#todomenu").css("visibility","visible");
+	$("#todomenu").empty();//emptyで子要素の削除
+// 	$("#todomenu").append("<li>しかし回り込まれてしまった。</li>");
 }
 
 //道具ボタンプッシュ時の処理
-function itemBTN_pushed():void{
-
+function magicBTN_pushed(){
+	$("#todomenu").css("visibility","visible");
+	$("#todomenu").empty();//emptyで子要素の削除
+	$("#todomenu").append("<li>ゆうしゃは　メラゾーマをとなえた。</li>");
+	$("#todomenu").append("<li>しかし　なにもおきなかった</li>");
 }
