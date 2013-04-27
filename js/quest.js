4551b53f31a221d9402b6dfaa4ab40141fffc0a4
phasmatodean <afuroman3104614@gmail.com>
2013-04-27 16:56:20 +0900

    demo

--- js/quest.js
+++ js/quest.js
@@ -1,19 +1,7 @@
-<<<<<<< HEAD
 function battleBTN_pushed() {
     $("#todomenu").css("visibility", "visible");
     $("#todomenu").empty();
     $("#todomenu").append("<li>仕事をする</li>");
-=======
-//たたかうボタンプッシュ時の処理
-function battleBTN_pushed(){
-	$("#todomenu").css("visibility","visible");
-	$("#todomenu").empty();//emptyで子要素の削除
-	
-	// for(var i:number=0; i < todo.length; i++){
-	// 	$("#todomenu").append("<li>" + todo[i] + "</li>");
-	// }
-	$("#todomenu").append("<li>TODOTODOTODO</li>");
->>>>>>> a3b66ec8813c78bf5012a84aefaa51e50c84bd50
 }
 function escapeBTN_pushed() {
     $("#todomenu").css("visibility", "visible");
@@ -30,14 +18,3 @@ function magicBTN_pushed() {
     $("#todomenu").append("<li>ゆうしゃは　メラゾーマをとなえた。</li>");
     $("#todomenu").append("<li>しかし　なにもおきなかった。</li>");
 }
-<<<<<<< HEAD
-=======
-
-//道具ボタンプッシュ時の処理
-function magicBTN_pushed(){
-	$("#todomenu").css("visibility","visible");
-	$("#todomenu").empty();//emptyで子要素の削除
-	$("#todomenu").append("<li>ゆうしゃは　メラゾーマをとなえた。</li>");
-	$("#todomenu").append("<li>しかし　なにもおきなかった</li>");
-}
->>>>>>> a3b66ec8813c78bf5012a84aefaa51e50c84bd50
